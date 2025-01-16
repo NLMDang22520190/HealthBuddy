dce0b07206aa5ad6827beea9e99d73fb3f5042fc
NLMDang22520190 <22520190@gm.uit.edu.vn>
2025-01-16 17:51:28 +0700

    Implement dark mode toggle and update Tailwind CSS configuration with new color palette; add AllUserRoutes component

--- healthbuddy.client/src/App.jsx
+++ healthbuddy.client/src/App.jsx
@@ -2,9 +2,36 @@ import { useEffect, useState, version } from "react";
 import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    let savedMode = localStorage.getItem("displayMode");
+    if (!savedMode) {
+      savedMode = "light";
+      localStorage.setItem("displayMode", savedMode);
+      setDarkMode(false);
+    }
+    setDarkMode(savedMode === "dark" ? true : false);
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    //localStorage.setItem("displayMode", darkMode ? "light" : "dark");
+  };
+
   return (
-    <div>
-      <h1>hi {version}</h1>
+    <div className={`${darkMode ? "dark" : ""}`}>
+      <div className="transistion-300 bg-white_smoke dark:bg-ebony h-screen flex items-center justify-center">
+        <h1 className="text-lg">hi {version}</h1>
+        <button
+          onClick={() => {
+            toggleDarkMode();
+          }}
+          className="size-28 bg-gradient-to-br from-primary-dark to-secondary-dark text-white"
+        >
+          test
+        </button>
+      </div>
     </div>
   );
 }
--- healthbuddy.client/tailwind.config.js
+++ healthbuddy.client/tailwind.config.js
@@ -3,11 +3,65 @@ const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 
 export default {
+  darkMode: "class",
   content: {
     files: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#99FFE0",
+          100: "#69FFD2",
+          200: "#39FFC4",
+          dark: "#09FFB5",
+          400: "#00D897",
+          500: "#00A876",
+          600: "#007854",
+          light: "#006043",
+          800: "#004832",
+          900: "#001811",
+        },
+        secondary: {
+          50: "#99EBFF",
+          100: "#69E1FF",
+          dark: "#39D7FF",
+          300: "#09CEFF",
+          400: "#00ADD8",
+          500: "#0086A8",
+          light: "#006078",
+          700: "#004D60",
+          800: "#003A48",
+          900: "#002630",
+        },
+        compleprimary: {
+          dark: "#FF99B8",
+          100: "#FF6996",
+          200: "#FF3974",
+          300: "#FF0953",
+          400: "#D80041",
+          light: "#C0003A",
+          600: "#A80032",
+          700: "#780024",
+          800: "#60001D",
+          900: "#480016",
+        },
+        complesecond: {
+          dark: "#FFAD99",
+          100: "#FF8769",
+          200: "#FF6139",
+          300: "#FF3A09",
+          400: "#D82B00",
+          light: "#C02600",
+          600: "#A82200",
+          700: "#781800",
+          800: "#601300",
+          900: "#480E00",
+        },
+        ebony: "#222428",
+        white_smoke: "#F5F5F5",
+      },
+    },
   },
   plugins: [flowbite.plugin()],
 };
