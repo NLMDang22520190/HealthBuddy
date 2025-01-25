528ba8793e629d174c5801ec35cd5c68b5f29a28
NLMDang22520190 <22520190@gm.uit.edu.vn>
2025-01-26 06:55:38 +0700

    Add IsApproved column to ExerciseType table in the database

---
+++ HealthBuddy.Server/Migrations/20250125234629_addisapprovedtoexercisetype.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthBuddy.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addisapprovedtoexercisetype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "ExerciseType",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "ExerciseType");
+        }
+    }
+}
