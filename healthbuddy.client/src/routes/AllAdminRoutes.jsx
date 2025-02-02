ddfdfcfdc159bda56dca9f9b8e3520f54aea4d98
NLMDang22520190 <22520190@gm.uit.edu.vn>
2025-02-02 09:31:59 +0700

    Add Post Management page and Top Menu component; update admin routes

--- healthbuddy.client/src/routes/AllAdminRoutes.jsx
+++ healthbuddy.client/src/routes/AllAdminRoutes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "../pages/Admin/Dashboard/Dashboard";
+import PostManagement from "../pages/Admin/PostManagement/PostManagement";
 
 const AllAdminRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/admin/posts" element={<PostManagement />} />
     </Routes>
   );
 };
